25cf773f842b74657d0165b9aacdd292439df85a
Ryan Posthumus <ryanposthumus19@gmail.com>
2015-08-30 13:35:11 -0700

    add refactored legislator helpers and updated show route on legisltr

--- app/controllers/legislators_controller.rb
+++ app/controllers/legislators_controller.rb
@@ -3,25 +3,26 @@ class LegislatorsController < ApplicationController
   include Sunlight
 
   def index
-    @lat_lng = cookies[:lat_lng].split("|")
-    @legislators = legislators_at(@lat_lng[0], @lat_lng[1])
-    # @legislators = legislators_in(49441)
+    if params[:zip]
+      location_data = {zip: params[:zip]}
+      @legislators = legislators_at(location_data)
+    else
+      @lat_lng = cookies[:lat_lng].split("|")
+      location_data = {lat: @lat_lng[0], lng: @lat_lng[1]}
+      @legislators = legislators_at(location_data)
+    end
     render json: @legislators
   end
 
   def show
-
+    @legislator = Legislator.find_by(bioguide_id: params[:id])
+    render json: @legislator
   end
 
 private
 
-  def legislators_at(lat,lng)
-    ids = rep_ids_at(lat,lng)
-    local_legislators_with(ids)
-  end
-
-  def legislators_in(zip)
-    ids = rep_ids_in(zip)
+  def legislators_at(location_data)
+    ids = rep_ids_at(location_data)
     local_legislators_with(ids)
   end
 
@@ -29,24 +30,18 @@ private
     reps = Legislator.where(:bioguide_id => ids)
   end
 
-  def rep_ids_in(zip)
-    rep_data = sunlight_api_query_in(zip)
+  def rep_ids_at(location_data)
+    rep_data = sunlight_api_query_at(location_data)
     ids = get_ids_from(rep_data)
   end
 
-  def rep_ids_at(lat,lng)
-    rep_data = sunlight_api_query_at(lat,lng)
-    ids = get_ids_from(rep_data)
-  end
-
-  def sunlight_api_query_in(zip)
+  def sunlight_api_query_at(location_data)
     client = create_sunlight_connection
-    local_reps_raw_data = client.local_legislators_in(zip)
-  end
-
-  def sunlight_api_query_at(lat, lng)
-    client = create_sunlight_connection
-    local_reps_raw_data = client.local_legislators_at(lat,lng)
+    if location_data[:zip]
+      local_reps_raw_data = client.local_legislators_in(location_data[:zip])
+    else
+      local_reps_raw_data = client.local_legislators_at(location_data[:lat],location_data[:lng])
+    end
   end
 
   def get_ids_from(rep_data)
