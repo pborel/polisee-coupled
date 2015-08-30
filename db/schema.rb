d8279132557538b0905d910404c2d1f4e46a9c9a
Ryan Posthumus <ryanposthumus19@gmail.com>
2015-08-30 15:16:49 -0700

    add favorite migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150830014712) do
+ActiveRecord::Schema.define(version: 20150830215923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: true do |t|
+    t.integer  "user_id"
+    t.string   "record_type"
+    t.datetime "notify_at"
+    t.string   "external_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "legislators", force: true do |t|
     t.string   "bioguide_id"
