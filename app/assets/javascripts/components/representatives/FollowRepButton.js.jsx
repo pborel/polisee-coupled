a509eeb17023ed5e721796b666369ca79647ea16
Emilio Corpus <emiliocorpus@gmail.com>
2015-08-31 14:47:27 -0700

    adds twitter, facebook, mail, link icons

--- app/assets/javascripts/components/representatives/FollowRepButton.js.jsx
+++ app/assets/javascripts/components/representatives/FollowRepButton.js.jsx
@@ -4,6 +4,8 @@ var FollowRepButton = React.createClass({
 			<div className="card-action debugger">
 				<a href={this.props.tweet_link}className="waves-effect btn-flat-large rep-follow-button"> <img src="https://maxcdn.icons8.com/Color/PNG/48/Social_Networks/twitter-48.png" title="Twitter" width="48" /> </a>
 				<a href={this.props.facebook_link} className="waves-effect btn-flat-large rep-follow-button"><img src="https://maxcdn.icons8.com/Color/PNG/48/Social_Networks/facebook-48.png" title="Facebook" width="48" /></a> 
+				<a href={this.props.contact_form} className="waves-effect btn-flat-large rep-follow-button"><img src="https://maxcdn.icons8.com/Color/PNG/48/City/post_office-48.png" title="Post Office" width="48"/></a> 
+				<a href={this.props.website} className="waves-effect btn-flat-large rep-follow-button"><img src="https://maxcdn.icons8.com/Color/PNG/48/Messaging/link-48.png" title="Link" width="48"/></a> 
 			</div>
 		)
 	}
@@ -14,5 +16,5 @@ var FollowRepButton = React.createClass({
 
 
 			
-
+			
 			
