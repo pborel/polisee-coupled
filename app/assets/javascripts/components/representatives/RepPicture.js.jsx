6a7a8a477a42cb2f6717cb0dd1b0244f3eb225ba
Emilio Corpus <emiliocorpus@gmail.com>
2015-08-31 13:13:11 -0700

    resizes card

--- app/assets/javascripts/components/representatives/RepPicture.js.jsx
+++ app/assets/javascripts/components/representatives/RepPicture.js.jsx
@@ -1,7 +1,7 @@
 var RepPicture = React.createClass({
 	render: function() {
 		return (
-		<div className="card-image waves-effect waves-block waves-light debugger circle">
+		<div className="card-image waves-effect waves-block waves-light debugger">
 			<img src={this.props.url} alt=""  className="rep-image activator" />
 		</div>
 		)
