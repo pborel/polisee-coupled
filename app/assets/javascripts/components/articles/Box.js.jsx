f62077097bdd7939dd6cdb35ab1bb21813b2cbb9
Phil Borel <email": null>
2015-08-31 17:37:10 -0700

    comment out console tests

--- app/assets/javascripts/components/articles/Box.js.jsx
+++ app/assets/javascripts/components/articles/Box.js.jsx
@@ -7,6 +7,7 @@ var Box = React.createClass({
   },
 
   loadArticlesFromServer: function(tab) {
+    // console.log(tab)
     $.ajax({
       tabName: tab,
       url: this.props.url,
@@ -28,9 +29,8 @@ var Box = React.createClass({
   },
 
   updateListView: function(tab) {
-    console.log("hitting Box")
+    // console.log("hitting Box")
     this.setState({currentTab: tab})
-    console.log(this.state.currentTab)
     this.loadArticlesFromServer(tab);
   },
 
