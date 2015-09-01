9d77bd7381421080bea0cf15d08be25ac630297e
Emilio Corpus <emiliocorpus@gmail.com>
2015-08-31 20:28:38 -0700

    wow phil is a huge b

--- app/assets/javascripts/components/layout/Menu.jsx
+++ app/assets/javascripts/components/layout/Menu.jsx
@@ -12,17 +12,41 @@ var Menu = React.createClass({
 
     return (
       <nav>
-        <span id="menu-link"><span></span></span>
         <div className="nav-wrapper">
-          <a href="#" className="brand-logo">Polisee</a>
-          <ul id="nav" className="right hide-on-med-and-down">
-            <li>Home</li>
-            <li>About</li>
-            {signingLink}
-          </ul>
+          <a href="#!" className="brand-logo center">Polisee</a>
+            <ul className="left hide-on-med-and-down">
+              <li>{signingLink}</li>
+            </ul> 
+            <ul className="right hide-on-med-and-down">
+              <form>
+                <div className="input-field">
+                  <input id="search" type="search" placeholder = "zipjawn"/>
+                  <label for="search"><i className="material-icons">search</i></label>
+                </div>
+              </form>
+            </ul>
         </div>
       </nav>
     );
   }
 });
 // auth/twitter/callback
+
+
+
+// <span id="menu-link"><span></span></span>
+// <div className="nav-wrapper">
+//   <a href="#" className="brand-logo">Polisee</a>
+//   <form>
+       
+//            <input id="search" type="search" required />
+//            <label for="search"><i className="material-icons">search</i></label>
+//            <i className="material-icons">close</i>
+//          </div>
+//   </form>
+//   <ul id="nav" className="right hide-on-med-and-down">
+//     <li>Home</li>
+//     <li>About</li>
+//     {signingLink}
+//   </ul>
+// </div>
