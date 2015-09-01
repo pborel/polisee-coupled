65fd4f4cc902791bcde8390f64878f4b74ee76df
Ryan Posthumus <ryanposthumus19@gmail.com>
2015-09-01 11:34:21 -0700

    commit to test legislatures by zip code functionality

--- app/controllers/legislators_controller.rb
+++ app/controllers/legislators_controller.rb
@@ -5,7 +5,6 @@ class LegislatorsController < ApplicationController
   def index
     p params
     if params[:zip]
-      
       location_data = {zip: params[:zip]}
       @legislators = legislators_at(location_data)
     else
