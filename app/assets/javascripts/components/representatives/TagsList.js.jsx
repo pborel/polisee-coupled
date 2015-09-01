3d74e0f6295fb99dd2d9a6f7b8ca52b3101fabd1
Emilio Corpus <emiliocorpus@gmail.com>
2015-08-31 19:11:01 -0700

    representative card with incomplete tags

---
+++ app/assets/javascripts/components/representatives/TagsList.js.jsx
@@ -0,0 +1,13 @@
+var TagsList = React.createClass({
+	render: function() {
+		return (
+		<div>
+<a class="btn tooltipped" data-position="bottom" data-delay="50" data-tooltip="Hi">Hover me!</a>
+		</div>
+		)
+	}
+})
+
+
+
+			
