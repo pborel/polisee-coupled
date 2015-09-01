c1d85974d9d4592415ae4da319efd6b5cf8e4acc
Michael Wong <contact.michaelwong@gmail.com>
2015-09-01 14:14:25 -0700

    find by zip works

--- app/assets/javascripts/app.js.jsx
+++ app/assets/javascripts/app.js.jsx
@@ -2,52 +2,11 @@ var RouteHandler = ReactRouter.RouteHandler,
   Link = ReactRouter.Link;
 
 var App = React.createClass({
-  getInitialState: function() {
-    return { signedIn: false,
-             representatives: null }
-  },
 
-  // handleSigningLink: function() {
-    // signedIn = !signedIn;
-  // },
-
-  setZipCode: function(zipCode) {
-    $.ajax({
-      url: "/legislators",
-      dataType: 'json',
-      cache: false,
-      data: {zip: zipCode},
-      success: function(reps) {
-        console.log("Zip: " + zipCode)
-        this.setState({representatives: reps});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.log("Zip: ERROR")
-        console.error('/check', status, err.toString());
-        console.error(this.state.zip);
-      }.bind(this)
-    });
-  },
-
-  checkSignedIn: function() {
-    $.ajax({
-      url: '/check',
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        this.setState({signedIn: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error('/check', status, err.toString());
-        console.error(this.state.data);
-      }.bind(this)
-    });
-  },
 
   render: function() {
     return (
       <div>
-        <Menu signedIn={this.state.signedIn} parentComponent={this} />
         <RouteHandler />
         <FooterContainer />
       </div>
