090e00a59e24fe5ca65daa842f6d278e98f326cd
Ryan Posthumus <ryanposthumus19@gmail.com>
2015-09-02 09:58:23 -0700

    refactor controllers for legislators, bills, favorites

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -4,28 +4,26 @@ class BillsController < ApplicationController
   def index
     path = params["tabName"]
     if path == "index" || path.nil?
-      sunlight_client = Congress.new
       @bills = JSON.parse((sunlight_client.bills).body)
     else path == "upcoming"
-      sunlight_client = Congress.new
       @bills = JSON.parse((sunlight_client.upcoming_bills).body)
     end
     render json: @bills["results"]
   end
 
   def show
-    sunlight_client = Congress.new
     @bill = sunlight_client.bill(params[:bill_id])
     render json: @bill
   end
 
   def search
-    p "*"* 50
-    p params
-    query = params[:query]
-    sunlight_client = Congress.new
-    @bills = sunlight_client.search_bills(query)
+    @bills = sunlight_client.search_bills(params[:query])
     render json: @bills
+  end
+
+  private
+  def sunlight_client
+    sunlight_client = Congress.new
   end
 
 end
