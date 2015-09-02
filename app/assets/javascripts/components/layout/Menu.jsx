418d009a28e646d6e6601d0e74baf63d5c4c24ba
Phil Borel <email": null>
2015-09-01 21:35:39 -0700

    fix menu styling

--- app/assets/javascripts/components/layout/Menu.jsx
+++ app/assets/javascripts/components/layout/Menu.jsx
@@ -9,15 +9,14 @@ var Menu = React.createClass({
 
   render: function() {
     if (this.props.signedIn) {
-      var signingLink = <li><span id={this.props.signedIn}><a href={'/signout'}><i className="material-icons">input</i></a></span></li>;
+      var signingLink = <li><span id={this.props.signedIn}><a href={'/signout'}><i className="material-icons">input</i>Sign Out</a></span></li>;
     } else {
-      console.log(this.props.parentComponent);
-      var signingLink = <li><span id={this.props.signedIn}><a href={'/auth/twitter'}><i className="material-icons">perm_identity</i></a></span></li>
+      var signingLink = <li><span id={this.props.signedIn}><a href={'/auth/twitter'}><i className="material-icons">perm_identity</i>Sign In</a></span></li>
     }
     return (
       <nav>
-        <div className="nav-wrapper">
-            <div className="container margin-padding debugger">
+        <div className="inner-menu-container">
+            <div className="debugger">
              <div className="row margin-padding debugger ">
                <div className="col margin-padding debugger">
                  <a href="/"><img className="brand-logo center logo debugger" src="https://slack-files.com/files-tmb/T09P4E2TZ-F0A0TB8L9-65253eb432/polisee_moniker_720.png"></img></a>
