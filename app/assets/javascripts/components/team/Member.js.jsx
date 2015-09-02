65d1d02a45af1aa08fe79cff0168d0c899e4466e
Phil Borel <email": null>
2015-09-02 16:43:58 -0700

    add team bio

--- app/assets/javascripts/components/team/Member.js.jsx
+++ app/assets/javascripts/components/team/Member.js.jsx
@@ -1,7 +1,7 @@
 var Member = React.createClass({
   render: function() {
     return (
-      <div className="card rep-card hoverable">
+      <div className="card rep-card hoverable left-align">
         <TeamPicture url={this.props.data.image_url} />
         <TeamName url={this.props.data.website_url} name={this.props.data.name} />
         <TeamButtonSpace data={this.props.data} />
