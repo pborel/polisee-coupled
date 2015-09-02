597f1a665e934dafdbc1814ab3de6737fd91b912
Emilio Corpus <emiliocorpus@gmail.com>
2015-09-02 10:45:03 -0700

    edits search functionality, search dynamically filters current index tab view

--- app/assets/javascripts/components/articles/Box.js.jsx
+++ app/assets/javascripts/components/articles/Box.js.jsx
@@ -7,47 +7,73 @@ var ArticleBox = React.createClass({
     };
   },
 
-  handleQueryChangeSubmit: function(query) {
-    var regExp = new RegExp(query)
-    var bills = this.state.originalList
-    var newData = []
-
-    for (var index in bills) {
-      if (bills[index].short_title === null) {
-        if (bills[index].official_title.toLowerCase().match(regExp)) {
-          newData.push(bills[index])
-        }
-      }
-      else {
-        if (bills[index].short_title.toLowerCase().match(regExp)) {
-          newData.push(bills[index])
-        }
-      }
-    }
-
-    if (newData.length > 0) {
-      this.setState({data: newData})
-    }
-  },
-
+  // SEARCH FILTER AND SUBMIT METHODS
   handleSearchSubmit: function(query) {
-
-    $.ajax({
-      url: '/bills/search',
-      dataType: 'json',
-      data: {query: query},
-      cache: false,
-      success: function(data) {
-      
-        this.setState({data: data.results});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error('/check', status, err.toString());
-        console.error(this.state.data);
-      }.bind(this)
-    });
+    if (query === "") {
+      this.handleEmpty(query)
+    }
+    else {
+      $.ajax({
+        url: '/bills/search',
+        dataType: 'json',
+        data: {query: query},
+        cache: false,
+        success: function(data) {
+            if (data.count === 0) {
+              console.log("no data found")
+              this.setState({data: this.state.originalList})
+            }
+            else {
+              this.setState({data: data.results});
+            }
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error('bills/search', status, err.toString());
+          console.error(this.state.data);
+        }.bind(this)
+      });
+    }
   },
 
+  handleQueryChange: function(query) {
+    var regEx = new RegExp(query)
+    var bills = this.state.data
+    var newBills = []
+    for (var index in bills) {
+      this.searchFor(bills[index], regEx, newBills)
+    }
+    this.handleFound(newBills)
+    if (query === "") {
+          this.handleEmpty(query)
+    }
+  },
+
+  searchFor: function(bill, query, foundBills) {
+    if (bill.short_title === null) {
+        if (bill.official_title.toLowerCase().match(query)) {
+          foundBills.push(bill)
+        }
+    }
+    else {
+        if (bill.short_title.toLowerCase().match(query)) {
+            foundBills.push(bill)
+        }
+    }
+  },
+
+  handleFound: function (foundBills) {
+    if (foundBills.length > 0) {
+      this.setState({data: foundBills})
+    }
+  },
+
+  handleEmpty: function (query) {
+      this.setState({data: this.state.originalList})
+  },
+
+
+
+  // INITIAL LOAD
   loadArticlesFromServer: function(tab, link) {
 
     $.ajax({
@@ -80,7 +106,7 @@ var ArticleBox = React.createClass({
   render: function() {
     return (
       <div className="debugger articles-box">
-        <SearchFilter handleQueryChange={this.handleQueryChangeSubmit} handleSearch={this.handleSearchSubmit}/>
+        <SearchFilter parentComponent={this}/>
         <Tabs parentElement={this} handleClick={this.updateListView} />
         <ArticleList data={this.state.data} favoritesUrl={this.props.favoritesUrl} />
       </div>
--- app/assets/javascripts/components/layout/SearchFilter.js.jsx
+++ app/assets/javascripts/components/layout/SearchFilter.js.jsx
@@ -3,20 +3,23 @@ var SearchFilter = React.createClass({
   submitChange: function (event) {
     event.preventDefault()
     var searchInput = React.findDOMNode(this.refs.search).value.toLowerCase()
-    this.props.handleQueryChange(searchInput)
+    this.props.parentComponent.handleQueryChange(searchInput)
   },
 
   submitQuery: function(event) {
     event.preventDefault()
     var searchInput = React.findDOMNode(this.refs.search).value
-    this.props.handleSearch(searchInput)
+    React.findDOMNode(this.refs.search).value = ''
+    this.props.parentComponent.handleSearchSubmit(searchInput)
   },
+
+
 
   render: function() {
     return (
       <nav className="cyan lighten-2">
         <div className="nav-wrapper search-bar tab-search">
-          <form onSubmit={this.submitQuery}>
+          <form onSubmit={this.submitQuery} >
               <div className="input-field">
                 <input id="search" type="search" placeholder="Search" ref="search" onChange={this.submitChange} required/>
                 <label forHTML="search"><i className="material-icons">search</i></label>
